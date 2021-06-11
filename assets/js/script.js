4cefd9d1f6b20bec4a6d270613f221cf669583f1
Maria Nery <67118326+mnvnery@users.noreply.github.com>
2021-06-11 13:21:07 +0100

    js fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,15 @@ $( ".hamburger" ).click(function() {
     $(".menu").toggle();
     // Do something else, like open/close menu
 });
+$( ".more-info" ).click(function() {
+    // Toggle class "is-active"
+    $(".mais-info").toggleClass('hidden');
+    // Do something else, like open/close menu
+});
+
+
+$( ".info-close" ).click(function() {
+    // Toggle class "is-active"
+    $(".mais-info").toggleClass('hidden');
+    // Do something else, like open/close menu
+});
